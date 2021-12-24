36e90d7414804551144ae1a965abfad522d07849
zmasis <96493909+zmasis@users.noreply.github.com>
2021-12-24 21:23:14 +0100

    Update test_generator.py

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -18,5 +18,3 @@ def test_sample_multiple_words():
 def test_generate_buzz_of_at_least_five_words():
     phrase = generator.generate_buzz()
     assert len(phrase.split()) >= 5
-    
-# Masis & Kale
