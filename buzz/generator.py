387a6426b07c64ee3993d41e70a539f663c0b158
Fouad-MHU <96632625+Fouad-MHU@users.noreply.github.com>
2021-12-24 20:29:31 +0000

    Update generator.py

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,12 +1,10 @@
 from __future__ import print_function
 import random
-import unittest
 
 buzz = ('continuous testing', 'continuous integration', 'continuous deployment', 'continuous improvement', 'devops')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly', 'seriously')
 verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
-
 
 
 def sample(lint, nummer = 1):
@@ -18,14 +16,12 @@ def sample(lint, nummer = 1):
     return result
 
 
-
 def generate_buzz():
     buzz_terms = sample(buzz, 2)
     
     phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs), sample(verbs), buzz_terms[1]])
     
     return phrase.title()
-
 
 
 if __name__ == "__main__":
